96fc38739477d40b3eeb4949ad6bf81e339ae221
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-11 18:28:53 +0200

    Settings form ready

--- src/components/TimerSettings.js
+++ src/components/TimerSettings.js
@@ -1,33 +1,92 @@
 import React from 'react';
-import { Modal, Button, Header, Dropdown } from "semantic-ui-react";
+import { Modal, Button, Form, Input } from "semantic-ui-react";
 
 class TimerSettings extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            prepareTime: 0,
-            timeLeft: 0,
-            baseRest: 0,
-			reduceRestByPercent: 50,
-            cycles: 0
+            prepMinutes: 0,
+            prepSeconds: 5,
+            climbMinutes: 2,
+            climbSeconds: 0,
+            restMinutes: 2,
+            restSeconds: 0,
+            cycles: 8,
+            cutRestByPercent: 50
         };
         
     };
 
+    handleSubmit = () => {
+        console.log(this.state)
+    }
 
+    handleChange = (e, { name, value }) => this.setState({ [name]: Number(value) })
 
     render() {
-        
+
         return (
-            <Modal trigger={<Button>Set Interval</Button>}>
+            <Modal trigger={<Button>Set default settings</Button>} centered={false} size={'tiny'} >
                 <Modal.Header>Settings:</Modal.Header>
                 <Modal.Content>
-                <Modal.Description>
-                    <Header>Default Profile Image</Header>
-                    <p>We've found the following gravatar image associated with your e-mail address.</p>
-                    <p>Is it okay to use this photo?</p>
-                </Modal.Description>
+                    <Form size='small' unstackable widths='equal' onSubmit={this.handleSubmit}>
+                        <Form.Group inline>
+                            <Form.Field>
+                                <label>Preparation: </label>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="prepMinutes" placeholder='0 min' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="prepSeconds" placeholder='5 sec' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                        </Form.Group>
+
+                        <Form.Group inline>
+                            <Form.Field>
+                                <label>Climbing: </label>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="climbMinutes" placeholder='2 min' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="climbSeconds" placeholder='0 sec' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                        </Form.Group>
+
+                        <Form.Group inline>
+                            <Form.Field>
+                                <label>Resting: </label>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="restMinutes" placeholder='2 min' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input name="restSeconds" placeholder='0 sec' type='tel' onChange={this.handleChange}/>
+                            </Form.Field>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Input
+                                fluid
+                                name="cycles"
+                                label='Cycles:'
+                                placeholder='8 cycles'
+                                onChange={this.handleChange}
+                            />
+                            <Form.Input
+                                fluid
+                                name="cutRestByPercent"
+                                label='Cut rest by % each cycle'
+                                placeholder='50%'
+                                onChange={this.handleChange}
+                            />
+                        </Form.Group>
+
+                        <Button type='submit'>Submit</Button>
+
+                    </Form>
                 </Modal.Content>
             </Modal>
         );
