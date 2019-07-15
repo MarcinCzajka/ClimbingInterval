6def6f0ba27d99db59caacbf1a4406c5a6f6749f
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-15 17:22:55 +0200

    Removed css method to block orientation change as it leaves browser menu bar visible and it look ugly. Used orientation.lock() instead

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+window.screen.orientation.lock("portrait-primary");
+
+async function lockOrientation() {
+    try {
+        await window.screen.orientation.lock("portrait-primary");
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+lockOrientation();
+
 ReactDOM.render(<App />, document.querySelector("#root"));
