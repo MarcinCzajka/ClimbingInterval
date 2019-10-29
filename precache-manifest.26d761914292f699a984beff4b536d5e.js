1a0908790226b03fd1a09846a4d0d2bf7b8eea45
Marcin Czajka <czajka.marcin@protonmail.com>
2019-10-29 09:36:12 +0100

    Created build

---
+++ precache-manifest.26d761914292f699a984beff4b536d5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c5002d32b4aaae6fa89a7ae7969355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca555d87ace70548b97f",
+    "url": "/static/css/main.29de404c.chunk.css"
+  },
+  {
+    "revision": "7daa43c20ae84e255c3f",
+    "url": "/static/js/2.fb9a7cf9.chunk.js"
+  },
+  {
+    "revision": "ca555d87ace70548b97f",
+    "url": "/static/js/main.c656359e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
