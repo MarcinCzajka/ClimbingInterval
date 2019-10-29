5b189ecc2d0e6fd4a1eba3fc4affa9d1738c66fb
Marcin Czajka <czajka.marcin@protonmail.com>
2019-10-29 17:32:16 +0100

    .

---
+++ build/precache-manifest.0d57e94ae5f5b263348a58d4a67d597f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a1f0b0c70db01d42a2f6ffd595a033",
+    "url": "/Klimb/index.html"
+  },
+  {
+    "revision": "66ed080f5a67c1103cc2",
+    "url": "/Klimb/static/css/main.29de404c.chunk.css"
+  },
+  {
+    "revision": "deb10014979747bcc9c6",
+    "url": "/Klimb/static/js/2.b348e9a8.chunk.js"
+  },
+  {
+    "revision": "66ed080f5a67c1103cc2",
+    "url": "/Klimb/static/js/main.4035e344.chunk.js"
+  },
+  {
+    "revision": "6387798071a4c7787d4f",
+    "url": "/Klimb/static/js/runtime~main.0dd08a56.js"
+  }
+]);
